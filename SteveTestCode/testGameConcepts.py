e55875015ad90e5507dc98a1463c76375713c0e2
stevenjgore <59741940+stevenjgore@users.noreply.github.com>
2020-01-17 22:44:05 -0800

    Update

--- SteveTestCode/testGameConcepts.py
+++ SteveTestCode/testGameConcepts.py
@@ -8,29 +8,43 @@ win = pygame.display.set_mode((screenWidth,screenHeight))
 
 pygame.display.set_caption("Test Game Concepts")
 
-bg = pygame.image.load('bg.jpg')
+class player(object):
+    ''' Player class. Defaulting attributes:
+    x = 50
+    y = 50
+    width = 25
+    height = 25
+    vel = 2
+    '''
+    def __init__(self, x, y, width, height, vel):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = vel
 
-#Player Helo Values
-x = 50
-y = 50
-width = 25
-height = 25
-vel = 2
+    def draw(self, win):
+        win.fill((0, 0, 0))
+        pygame.draw.rect(win, (0, 0, 255), (self.x, self.y, self.width, self.height))
 
-#Gun Values
-xGun = x
-yGun = y
-gunWidth = 5
-gunHeight = 5
-gunVel = 10
+class groundDino(object): #TODO: Just created groundDino class. Need to go through MAIN to update w/ OOP
+    '''Ground Dinosaur. Defaulting attributes:
+    width1 = 30
+    height1 = 30
+    x1 = 5
+    y1 = screenHeight - height1
+    vel1 = 0.5
+    x1Mod = 1 '''
+    def __init__(self, x, y, width, height, vel):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = vel
+        self.xMod = 1
 
 #Ground Dino 1 Values
-width1 = 30
-height1 = 30
-x1 = 5
-y1 = screenHeight - height1
-vel1 = 0.5
-x1Mod = 1
+
 
 #Ground Dino 2 Values
 width2 = 15
@@ -51,9 +65,8 @@ airYMod = -1
 
 def redrawGameWindow():
     win.fill((255, 255, 255))
-
     #draw player helo
-    pygame.draw.rect(win,(0,0,255),(x,y,width,height))
+    player1.draw(win)
     #draw dino1
     pygame.draw.rect(win,(255,0,0),(x1,y1,width1,height1))
     #draw dino2
@@ -63,7 +76,8 @@ def redrawGameWindow():
 
     pygame.display.update()
 
-#######################MAIN LOOP#################################
+#######################  MAIN  #################################
+player1 = player(50, 50, 25, 25, 2)
 run = True
 while run:
     pygame.time.delay(25)
@@ -102,20 +116,23 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and x > vel:
-        x -= vel
-    if keys[pygame.K_RIGHT] and x < (screenWidth - width):
-        x += vel
-    if keys[pygame.K_UP] and y > vel:
-        y -= vel
-    if keys[pygame.K_DOWN] and y < (screenHeight - height):
-        y += vel
+    if keys[pygame.K_LEFT] and player1.x > player1.vel:
+        player1.x -= player1.vel
+    if keys[pygame.K_RIGHT] and player1.x < (screenWidth - player1.width):
+        player1.x += player1.vel
+    if keys[pygame.K_UP] and player1.y > player1.vel:
+        player1.y -= player1.vel
+    if keys[pygame.K_DOWN] and player1.y < (screenHeight - player1.height):
+        player1.y += player1.vel
     if keys[pygame.K_SPACE]:
         #TODO: shoot gun
+        '''
         xGun = x
         yGun = y + (height / 2)
         pygame.draw.rect(win,(0,0,0),(xGun,yGun,screenWidth,2))
         pygame.display.update()
+        '''
+        pass
 
     redrawGameWindow()
 
