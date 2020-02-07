21901b3db1e88e5612db243b4f5312a3c004eacc
neuroblastoma <neuroblastoma@users.noreply.github.com>
2020-02-06 22:02:38 -0800

    Update Dino_Hunter.py
    Moved MAIN CODE to main()

--- Dino_Hunter.py
+++ Dino_Hunter.py
@@ -11,17 +11,9 @@
 # John Lytle
 
 import pygame
-pygame.init()
-
-screenWidth = 1500
-screenHeight = 750
-win = pygame.display.set_mode((screenWidth, screenHeight))
-
-pygame.display.set_caption("Dino Hunter")
+import sys
 
 # CLASSES ##################
-
-
 class Entity(object):
     """This is the top-level class for any character entity that will exist on the screen.
     Attributes: health, width, height, x-position, y-position, and velocity.
@@ -36,43 +28,104 @@ class Entity(object):
         self.height = height
         self.vel = vel
 
-    # def draw(self, win):
+    def draw(self, win):
         # TODO: Do we need a draw to window method in the Entity class?
+        pass
 
     # def bound(self):
         # TODO: Do we need a bound to window method in the Entity class?
 
+
+class Player(Entity):
     # TODO: Player (sub)Class
     # Attributes: Lives, Weapons/Power-ups
     # Rules: Clipping/Sprite collision: player will lose health if collides with any other object
+    def __init__(self):
+        super().__init__(health=100, x=0, y=0, width=5, height=5, vel=0)
 
+        #self.image or spritesheet
+        #self.rect
+
+
+        self.lives = 3
+
+    # def draw(self, win):
+    #     """Blit the player to the window"""
+    #     win.blit(self.image, self.rect)
+
+    # def check_collision(self):
+    #     return NotImplentedError
+
+# class AirDino(Entity):
     # TODO: Air/Ground Dinosaur Class
     # Attributes: ?
     # Behaviors: ?
     # Rules: ?
 
+
 # TODO: Projectile Class outside of Entity class?
 
 
-def redrawGameWindow():
+# class ControlManager(object):
+    # """Class for tracking game states & managing event loop
+    #     https://github.com/Mekire/pygame-samples/blob/master/platforming/moving_platforms.py
+    # """
+#     def __init__(self):
+#         """Initialize the diplay and prepare game objects"""
+#         self.screen = pygame.display.get_surface()
+#         self.screen_rect = self.screen.get_rect()
+#         self.clock = pygame.time.Clock()
+#         self.fps = 60
+#         self.keys = pygame.key.get_pressed()
+#         self.done = False
+#         self.player = Player()
+#         self.viewport = self.screen.get_rect()
+
+
+#     def update_viewport(self):
+#         """The viewport stays centered on player 
+#         unless the player is at the edge of the screen."""
+#         self.viewport.center = self.player.rect.center
+#         self.viewport.clamp_ip(self.level_rect)
+
+def redrawGameWindow(win):
     """redrawGameWindow function will fill the window with the specific RGB value and then call on each
     object's .draw() method in order to populate it to the window. """
     win.fill((255, 255, 255))
 
-# MAIN CODE ################################################################################################
-# instantiate player
-# instantiate dinosaurs
 
-run = True
-while run:
-    pygame.time.delay(25)
+def main():
+    # MAIN CODE ################################################################################################
+    
+    # May want to consider moving this to its own DinoGame class (2/6: Lytle)
+    pygame.init()
 
-    # main game code
+    screenWidth = 1500
+    screenHeight = 750
+    win = pygame.display.set_mode((screenWidth, screenHeight))
+    pygame.display.set_caption("Dino Hunter")
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
 
-    redrawGameWindow()
 
-pygame.quit()
+    # instantiate player
+    # instantiate dinosaurs
+
+    run = True
+    while run:
+        pygame.time.delay(25)
+
+        # main game code
+
+        # check events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+
+        redrawGameWindow(win)
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
