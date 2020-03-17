5eec60077c18399b55a0893c3a752d25f333c1ce
neuroblastoma <neuroblastoma@users.noreply.github.com>
2020-03-16 20:01:57 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Dino_Hunter',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/neuroblastoma/Dino_Hunter',
+    license='',
+    author='',
+    author_email='',
+    description='Defender clone with dinosaurs',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3'
+    ],
+)
+
